3d2d019d261ab20d0a5e634d05c2d4f5b38cee3a
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2023-12-31 04:47:29 +0600

    added comment in handleGuesses.js

--- gameFunctions/handleGuesses.js
+++ gameFunctions/handleGuesses.js
@@ -1,18 +1,20 @@
 const getCodeBlock = require("../utils/getCodeBlock.js");
 const makeEmbed = require("../utils/makeEmbed.js");
-
+// matches correct word and guessed word and shows final result
 function handleGuesses(e, guessedWord, pickedWord, triesLeft, gameFinished = false){
 
 	let colorArr = [];
-
+	// matches each letter and sets their color in colorArr based of conditions
 	for(let i=0; i<guessedWord.length; i++){
-
+		// color green if correct letter in correct position
 		if(guessedWord[i] === pickedWord[i]){
 			colorArr.push("green");
 		}
+		// color orange if correct letter in wrong place
 		else if(pickedWord.includes(guessedWord[i])){
 			colorArr.push("orange");
 		}
+		// color white if wrong letter
 		else{
 			colorArr.push("white");
 		}
@@ -21,7 +23,7 @@ function handleGuesses(e, guessedWord, pickedWord, triesLeft, gameFinished = fal
 	const CODE_BLOCK = getCodeBlock(guessedWord , colorArr, triesLeft, gameFinished);
 	const EMBED = makeEmbed(CODE_BLOCK, triesLeft);
 	e.channel.send({embeds: [EMBED]});
-
+	
 	if(gameFinished){
 		colorArr = [];
 	}
