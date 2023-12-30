454d5b0636297dab318a47967b26bc11cd715f5b
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2023-12-31 04:33:05 +0600

    added comment in setCodeBlock.js

--- utils/setCodeBlock.js
+++ utils/setCodeBlock.js
@@ -1,19 +1,21 @@
+// recieves an array of all the words player guessed and returns them all in a code block with some slyling
 function setCodeBlock(guessedWordsArr){
-  // the following string would look like this
-  /* 
-    ```ansi
-      \u001b[0;0m \u001b[1;31mExample
-    ```
-  */
-
+	
 	let text = "";
 	guessedWordsArr.forEach((word) => {
+		// styles each word by putting some space at the beginnning and end(\t) and each word in a new line(\n)
 		text += `|\t${word}\t|\n`;
-		if(word !== guessedWordsArr[guessedWordsArr.length - 1])
-			text += "\n";
 	});
-	// return `\`\`\`ansi\n${text}\`\`\``
-	
+
+// this particular code is unindented cause this will be sent to discord as a message
+//  and since i am using string literals the spaces added as indentation will also be accounted for when sending the message
+// thus it'll ruin the structure of the code block
+// in this case the following string would look like this when sent to discord
+/* 
+```ansi
+  \u001b[0;0m \u001b[1;31m< This is just ANSI code for coloring letters >
+```
+*/
   return`
 \`\`\`ansi
 ${text}
