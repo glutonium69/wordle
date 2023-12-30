e8bf62ec83709f8772c7688f4fc9c70358c689b4
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2023-12-31 03:53:50 +0600

    added comments to colorLetters.js

--- utils/colorLetters.js
+++ utils/colorLetters.js
@@ -1,9 +1,11 @@
-const colorText = require("./colorText.js");
-
+const getANSIcode = require("./getANSIcode.js");
+// function that takes a word and an array of string of colors that define what color each individual letters will be
+// returns a string where each letter of the word is wrapped around with ANSI code for different color based on the array
 function colorLetters(word, colorArr){
 	word = word.toUpperCase();
 	let letterArr = word.split("");
-	let coloredLettersArr = letterArr.map((letter, idx) => colorText(letter, colorArr[idx]));
+	// passes each letter to getANSIcode(), which returns ANSI code for each letter for their defined color
+	let coloredLettersArr = letterArr.map((letter, idx) => getANSIcode(letter, colorArr[idx]));
 	let coloredWord = coloredLettersArr.join(" ");
 	return coloredWord;
 }
