fe1f44dbbfbd6291f01e09cd669f089e5e253ea2
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2023-12-31 22:02:26 +0600

    Update getCodeBlock.js

--- utils/getCodeBlock.js
+++ utils/getCodeBlock.js
@@ -1,16 +1,11 @@
 const colorLetters = require("./colorLetters.js");
 const setCodeBlock = require("./setCodeBlock.js");
 
-function getCodeBlock(word, guessedWordArr, colorArr, triesLeft, gameFinished){
+function getCodeBlock(word, guessedWordArr, colorArr, triesLeft){
 	
 	const COLORED_WORD = colorLetters(word, colorArr);
 	guessedWordArr.push(COLORED_WORD);
 	const CODE_BLOCK = setCodeBlock(guessedWordArr);
-
-	if(gameFinished){
-		guessedWordArr = [];
-	}
-
 	return CODE_BLOCK;
 }
 
