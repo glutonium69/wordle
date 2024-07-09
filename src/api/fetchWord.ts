339126e77b777501b9bb0bbac5f27ff6a2e4710c
glutonium69 <tahzib404@gmail.com>
2024-07-09 06:24:00 +0600

    fetchWord api shatted. it no longer works. using a diff api
    Fun fact: i made this new API

--- src/api/fetchWord.ts
+++ src/api/fetchWord.ts
@@ -1,9 +1,10 @@
 export default async function fetchWord(wordLength: number): Promise<string | null> {
 	try {
-		const URL = "https://random-word-api.herokuapp.com/word?length=" + wordLength;
+		const URL = "https://randomwordapi.onrender.com/word/rand/" + wordLength;
 		const res = await fetch(URL)
+                if(!res.ok) return null;
 		const data = await res.json()
-		return data[0];
+		return data.word;
 	}
 	catch (err) {
 		console.log(err);
