27424b391e6bb02608aa934bf03ae49e5c59c8f1
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2024-07-13 23:17:17 +0600

    Update fetchWord.ts

--- src/api/fetchWord.ts
+++ src/api/fetchWord.ts
@@ -1,10 +1,18 @@
 export default async function fetchWord(wordLength: number): Promise<string | null> {
 	try {
-		const URL = "https://randomwordapi.onrender.com/word/rand/" + wordLength;
-		const res = await fetch(URL)
-                if(!res.ok) return null;
-		const data = await res.json()
-		return data.word;
+		const URL_PRIMARY = "https://random-word-api.herokuapp.com/word?length=" + wordLength;
+		const resPrimary = await fetch(URL_PRIMARY);
+                if(!resPrimary.ok) {
+			const URL_SECONDARY = "https://randomwordapi.onrender.com/word/rand/" + wordLength;
+			const resSecondary = await fetch(URL_SECONDARY);
+			if(!resSecondary.ok) {
+				return null;
+			}
+			const dataSecondary = await resSecondary.json()
+			return dataSecondary.word;
+		};
+		const dataPrimary = await resPrimary.json()
+		return dataPrimary[0];
 	}
 	catch (err) {
 		console.log(err);
